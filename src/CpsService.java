9895fc786b02b10d45a3be1bdf8bdef3f234f9e7
Zerodoom <zerodoom675@mail.ru>
2023-10-22 19:27:12 +0300

    second task

--- src/CpsService.java
+++ src/CpsService.java
@@ -3,9 +3,8 @@ public class CpsService {
             int sum,
             double percent,
             int time
-    )
-    {
-        return (int) (sum*percent/12*Math.pow((1+percent/12), time)/(Math.pow((1+percent/12), time)-1));
+    ) {
+        return (int) (sum * percent / 12 * Math.pow((1 + percent / 12), time) / (Math.pow((1 + percent / 12), time) - 1));
 
     }
 }
